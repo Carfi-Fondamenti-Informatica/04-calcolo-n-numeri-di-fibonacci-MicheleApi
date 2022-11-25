9b90d1c130f0bf0792f693f6b47e0065491720b7
MicheleApi <117985120+MicheleApi@users.noreply.github.com>
2022-11-25 12:12:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int fattoriale(int a){
-int risultato = 1;
-for (int i=1; i<=a; i++){
-    risultato = risultato*i;
-}
-return risultato;
-}
-
-int fattoriale2 (int a){
-    int risultato =1;
-    while (a>1) {
-        risultato *=a;
-        a--;
-    }
-    return risultato;
-}
-
-int fattoriale3 (int a){
-    int risultato = 1;
-    for (int i=a; i>0; i--){
-        risultato *=1;
-    }
-    return risultato;
-}
-
 int main() {
-    cout << "fatt(4)=" << fattoriale (4) << endl;
-    cout << "fatt (7)=" << fattoriale (7) << endl;
-    cout << "fatt (11)=" << fattoriale (11) << endl;
-    return 0;
+   int n=0, a=1, b=1, c=0;
+    cin >> n;
+    if(n>=2) {
+        cout << a << endl << b << endl;
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+            cout << c << endl;
+        }
+    }
+    else {
+        cout << "errore" << endl;
+    }
+   return 0;
 }
